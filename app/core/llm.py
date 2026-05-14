a28a65d56fff4c8bb75c9fee27a3443c83ada937
Pixie <eugenmac56@gmail.com>
2026-05-14 22:30:34 +0300

    доработка логики

--- app/core/llm.py
+++ app/core/llm.py
@@ -40,6 +40,7 @@ llm = ChatOllama(
     model=MBB_OLLAMA_MODEL_NAME,
     temperature=0.02,
     base_url="http://localhost:11434",
+    num_ctx=1024
 )
 log.info("Модель LLM инициализирована: %s", llm.model)
 log.info("Системный промпт и шаблон загружены.")
